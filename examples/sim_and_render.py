95737a62ba40a549d7a1c69a4d52d3bd4410b732
NetworkGestalt <68624142+NetworkGestalt@users.noreply.github.com>
2026-01-27 06:17:33 +0000

    Add files via upload

---
+++ examples/sim_and_render.py
@@ -0,0 +1,26 @@
+import numpy as np
+from pong import Pong
+from render import Renderer
+from animate import merge_crops, create_animation
+
+if __name__ == "__main__":
+    T = 100
+    env = Pong()
+    env.reset(seed=0)
+    renderer = Renderer(env.settings)
+
+    object_crops = []
+    full_frames = []
+
+    for _ in range(T):
+        state, info = env.step()
+        left, right, ball, score = renderer.render_crops(state=state,
+                                                         prev_state=env.prev_state,
+                                                         crop_size=20,
+                                                         supersample=8)
+        object_crops.append(merge_crops([left, right, ball, score]))
+        full_frame = renderer.reconstruct_frame(crops=(left, right, ball, score), state=state)
+        full_frames.append(full_frame)
+
+    anim = create_animation(full_frames)
+    anim.save("output.gif", writer="pillow", fps=24)     # requires Pillow package
