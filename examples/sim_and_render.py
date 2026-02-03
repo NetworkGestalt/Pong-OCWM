86485854c94f3b911ce18e5394c1fe45905f1ce4
NetworkGestalt <68624142+NetworkGestalt@users.noreply.github.com>
2026-02-03 18:31:07 +0000

    Update sim_and_render.py

--- examples/sim_and_render.py
+++ examples/sim_and_render.py
@@ -6,21 +6,19 @@ from animate import merge_crops, create_animation
 if __name__ == "__main__":
     T = 300
     env = Pong()
-    env.reset(seed=np.random.randint(1_000_000))
     renderer = Renderer(env.settings)
+    env.reset(seed=np.random.randint(1_000_000))
     
     object_crops = []
     full_frames = []
     
     for _ in range(T):
-        state, info = env.step()
+        state, info = env.step(left_action=None, right_action=None)
         left, right, ball, score = renderer.render_crops(state=state, prev_state=env.prev_state)
         object_crops.append(merge_crops([left, right, ball, score]))
         full_frame = renderer.reconstruct_frame(crops=(left, right, ball, score), state=state)
         full_frames.append(full_frame)
+        #print(info)
     
     anim = create_animation(full_frames)
-    HTML(anim.to_jshtml())
-
-    # To save as .gif (requires Pillow package):
-    # anim.save("pong.gif", writer="pillow", fps=24))
+    anim.save("pong.gif", writer="pillow", fps=24)   # requires Pillow package
